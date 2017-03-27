b62fb404326f2e262de9c759fafd25e80fa6f4ad
leemyeongjin3200 <lmg223@naver.com>
2017-03-27 17:05:41 +0900

    123

--- public/js/pushpotsSDK_test.js
+++ public/js/pushpotsSDK_test.js
@@ -11,7 +11,7 @@
             $.ajax({
                 url : "pushpotsdk_auth",
                 type : 'POST',
-                dataType : 'json',
+                dataType : 'jsonp',
                 data : {
                     user_key : user_key,
                     domain : user_domain
